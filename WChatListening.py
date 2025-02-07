5d20469027c7a546bb0b3ad7ae035f5f44f5a772
BingGS <qwq2086@qq.com>
2025-02-07 23:38:36 +0800

    v2.0

--- WChatListening.py
+++ WChatListening.py
@@ -1,7 +1,10 @@
+import os
+import time
 from wcferry import Wcf,WxMsg
 from queue import Empty
 from threading import Thread
-from link_model import respond
+from link_model import respond,respond_with_internet
+from json import loads,dumps
 
 memory = {}
 model = {}
@@ -10,7 +13,67 @@ num_context = {}
 temperature_room = {}
 addition = {}
 thought_output = {}
+prompt = {}
 HOLD_TAG = [False]
+models_dict = [
+    "deepseek-r1:1.5b",
+    "deepseek-r1:8b",
+    "deepseek-r1:14b",
+    "qwen:0.5b",
+    "qwen:4b",
+    "qwen:14b"
+]
+
+def file_save(path):
+    global memory
+    global temperature_room
+    global addition
+    global thought_output
+    global prompt
+    global num_context
+    global initialized
+    with open(os.path.join(path,"memory.json"),"w",encoding="utf-8") as f:
+        f.write(dumps(memory,ensure_ascii=False))
+    with open(os.path.join(path,"temperature_room.json"),"w",encoding="utf-8") as f:
+        f.write(dumps(temperature_room,ensure_ascii=False))
+    with open(os.path.join(path,"addition.json"),"w",encoding="utf-8") as f:
+        f.write(dumps(addition,ensure_ascii=False))
+    with open(os.path.join(path,"model.json"),"w",encoding="utf-8") as f:
+        f.write(dumps(model,ensure_ascii=False))
+    with open(os.path.join(path,"initialized.json"),"w",encoding="utf-8") as f:
+        f.write(dumps(initialized,ensure_ascii=False))
+    with open(os.path.join(path,"thought_output.json"),"w",encoding="utf-8") as f:
+        f.write(dumps(thought_output,ensure_ascii=False))
+    with open(os.path.join(path,"num_context.json"),"w",encoding="utf-8") as f:
+        f.write(dumps(num_context,ensure_ascii=False))
+    with open(os.path.join(path,"prompt.json"),"w",encoding="utf-8") as f:
+        f.write(dumps(prompt,ensure_ascii=False))
+
+def file_load(path):
+    with open(os.path.join(path,"memory.json"),"r",encoding="utf-8") as f:
+        global memory
+        memory = loads(f.read())
+    with open(os.path.join(path,"temperature_room.json"),"r",encoding="utf-8") as f:
+        global temperature_room
+        temperature_room = loads(f.read())
+    with open(os.path.join(path,"addition.json"),"r",encoding="utf-8") as f:
+        global addition
+        addition = loads(f.read())
+    with open(os.path.join(path,"model.json"),"r",encoding="utf-8") as f:
+        global model
+        model = loads(f.read())
+    with open(os.path.join(path,"initialized.json"),"r",encoding="utf-8") as f:
+        global initialized
+        initialized = loads(f.read())
+    with open(os.path.join(path,"thought_output.json"),"r",encoding="utf-8") as f:
+        global thought_output
+        thought_output = loads(f.read())
+    with open(os.path.join(path,"num_context.json"),"r",encoding="utf-8") as f:
+        global num_context
+        num_context = loads(f.read())
+    with open(os.path.join(path,"prompt.json"),"r",encoding="utf-8") as f:
+        global prompt
+        prompt = loads(f.read())
 
 def memo_add(role:str, content:str, roomid):
     memory[roomid].append({'role':role,'content':content})
@@ -19,20 +82,23 @@ def memo_add(role:str, content:str, roomid):
 
 def memo_clear(roomid):
     memory[roomid].clear()
-    memory[roomid].append({"role": "system","content": "请想象你现在加入了一个群聊，你需要对群聊里的消息进行回复。回复尽量简短，而且要求风趣贴心。"})
+    if "chatroom" in roomid:
+        memory[roomid].append({"role": "system","content": prompt[roomid]})
     # init(roomid, model=model[roomid])
 
 def memo_init(roomid):
-    memory[roomid] = [{"role": "system","content": "请想象你现在加入了一个群聊，你需要对群聊里的消息进行回复。回复尽量简短，而且要求风趣贴心。"}]
-    model[roomid] = "deepseek-r1:8b"
+    prompt[roomid] = "请想象你现在加入了一个群聊，你需要对群聊里的消息进行回复。回复尽量简短，而且要求风趣贴心。"
+    memory[roomid] = [{"role": "system","content": prompt[roomid]}]
+    model[roomid] = "deepseek-r1:1.5b"
     num_context[roomid] = 20
     temperature_room[roomid] = 0.0
-    addition[roomid] = " 请用纯文字回答，不含有任何格式，同时你的回答尽量简短。"
+    addition[roomid] = ""
     thought_output[roomid] = False
     # init(roomid,model=model[roomid])
     initialized.append(roomid)
 
 def memo_init_single(roomid):
+    prompt[roomid] = ""
     memory[roomid] = []
     model[roomid] = "deepseek-r1:8b"
     num_context[roomid] = 20
@@ -41,9 +107,9 @@ def memo_init_single(roomid):
     thought_output[roomid] = True
     initialized.append(roomid)
 
-def memo_reset(text : str, roomid):
-    memory[roomid].clear()
-    memory[roomid].append({"role": "user","content": text})
+def memo_del(num,roomid):
+    while num>0 and len(memory[roomid])>0:
+        del memory[roomid][-1]
 
 def order(message,msg,roomid,wcf):
     if message == "#Restart":
@@ -54,17 +120,18 @@ def order(message,msg,roomid,wcf):
     elif message.startswith("#ResetPrompt"):
         print("\033[34m[ORDER] ResetPrompt in {}.\033[0m".format(roomid))
         text = message[message.find("#ResetPrompt") + len("#ResetPrompt") + 1:]
-        memo_reset(text, roomid)
+        prompt[roomid] = text
         wcf.send_text("Reset prompt.", msg.roomid)
         return
     elif message.startswith("#ChooseModel"):
         print("\033[34m[ORDER] Changed Model in {}.\033[0m".format(roomid))
         text = message[message.find("#ChooseModel") + len("#ChooseModel") + 1:]
-        print("[Choose] {}".format(text))
-        if text != "deepseek-r1:14b" and text != "deepseek-r1:8b":
+        if text not in models_dict:
             wcf.send_text("No such model.", msg.roomid)
             return
         model[roomid] = text
+        if "qwen" in text:
+            thought_output[roomid] = False
         wcf.send_text("Changed Model.", msg.roomid)
         return
     elif message.startswith("#ContextNum"):
@@ -95,13 +162,20 @@ def order(message,msg,roomid,wcf):
         return
     elif message.startswith("#Thought"):
         print("\033[34m[ORDER] Set output_thought in {}.\033[0m".format(roomid))
+        if "qwen" in model[roomid]:
+            thought_output[roomid] = False
+            wcf.send_text("Thought is unavailable.", msg.roomid)
+            return
         text = int(message[message.find("#Thought") + len("#Thought") + 1:])
         thought_output[roomid] = text
         wcf.send_text(f"Thought: {"True" if text else "False"}.", msg.roomid)
         return
     elif message.startswith("#KillErr"):
         print("\033[34m[ORDER] KillErr in {}.\033[0m".format(roomid))
-        memo_init(roomid)
+        if "chatroom" in roomid:
+            memo_init(roomid)
+        else:
+            memo_clear(roomid)
         wcf.send_text("KillErr.", msg.roomid)
         return
     elif message.startswith("#HOLD"):
@@ -122,17 +196,37 @@ def order(message,msg,roomid,wcf):
         else:
             wcf.send_text("Illegal message.", msg.roomid)
             return
+    elif message.startswith("#Show"):
+        print("\033[34m[ORDER] Show Settings in {}.\033[0m".format(roomid))
+        wcf.send_text(f"Model:{model[roomid]} , "+
+                      f"NumContext:{num_context[roomid]} , "+
+                      f"Temperature:{temperature_room[roomid]} , "+
+                      f"Output_Thought:{True if thought_output[roomid] else False} , "+
+                      f"Addition:{addition[roomid] if addition[roomid] != "" else None} ", msg.roomid)
+        return
+    elif message.startswith("#Del"):
+        text = message[message.find("#Del") + len("#Del"):].replace(" ", "")
+        num = 2 if text == "" else int(text)
+        print("\033[34m[ORDER] Del {} conversations in {}.\033[0m".format(num,roomid))
+        memo_del(num,roomid)
+        wcf.send_text(f"Del {num}.", msg.roomid)
+        return
     else:
         print("\033[34m[ORDER] Illegal message sent in {}.\033[0m".format(roomid))
         wcf.send_text("Illegal message.", msg.roomid)
         return
 
-
 def processMsg(msg : WxMsg,wcf : Wcf):
     print("[INFO] Received message from RoomID:\033[4m{}\033[0m".format(msg.roomid))
     roomid = msg.roomid
-    if msg.is_at(wcf.get_self_wxid()) or ("chatroom" not in roomid):
+    if msg.is_at(wcf.get_self_wxid()) or ("chatroom" not in roomid) and msg.sender != wcf.self_wxid:
         message = msg.content[msg.content.find("@Bot")+len("@Bot")+1:] if "chatroom" in roomid else msg.content
+        if roomid not in initialized:
+            print("[INFO] Initializing {}.".format(roomid))
+            if "chatroom" in roomid:
+                memo_init(roomid)
+            else:
+                memo_init_single(roomid)
         if message.startswith("#"):
             order(message,msg,roomid,wcf)
             return
@@ -140,17 +234,20 @@ def processMsg(msg : WxMsg,wcf : Wcf):
             print("\033[30;44m[HOLD]\033[0m Refused requirement.")
             wcf.send_text("[HOLD]Now service is unavailable.",roomid)
             return
-        if roomid not in initialized:
-            print("[INFO] Initializing {}.".format(roomid))
-            if "chatroom" in roomid:
-                memo_init(roomid)
-            else:
-                memo_init_single(roomid)
         message = message + addition[roomid]
         memo_add("user", message, roomid)
-        result,thought = respond(model=model[roomid],
-                                 temperature=temperature_room[roomid],
-                                 memory=memory[roomid])
+        if message.startswith("*"):
+            memory[roomid][-1]["content"] = memory[roomid][-1]["content"].replace("*","")
+            result,thought = respond_with_internet(model=model[roomid],
+                                                   temperature=temperature_room[roomid],
+                                                   memory=memory[roomid])
+        else:
+            result,thought = respond(model=model[roomid],
+                                     temperature=temperature_room[roomid],
+                                     memory=memory[roomid])
+        if result is None:
+            wcf.send_text("bot似乎似了",roomid)
+            return
         memo_add("assistant", message, roomid)
         if thought_output[roomid] and len(thought) != 0:
             wcf.send_text(f"思考过程：{thought}", msg.roomid)
@@ -159,18 +256,27 @@ def processMsg(msg : WxMsg,wcf : Wcf):
 
 def enableReceiving(wcf : Wcf):
     def core():
+        t1 = time.time()
         while wcf.is_receiving_msg():
+            t2= time.time()
+            if t2-t1>1500:
+                print("[INFO] Save.")
+                file_save("Save")
+                t1=time.time()
             try:
                 processMsg(wcf.get_msg(),wcf)
             except Empty:
                 continue
             except Exception as e:
-                print("[ERROR] {}".format(e))
+                print("\033[30;41m[ERROR]\033[0m {}".format(e))
     wcf.enable_receiving_msg()
     Thread(target=core,name="WeChatBot",daemon=True).start()
     print("\033[3mBot running.\033[0m")
 
 if __name__ == "__main__":
+    path = "Save"
+    file_load(path)
+    print("\033[3mHistory loaded.\033[0m")
     wcf = Wcf()
     print("\033[3mWCF Generated.\033[0m")
     enableReceiving(wcf)
--- link_model.py
+++ link_model.py
@@ -1,4 +1,68 @@
+import copy
 import requests
+from fetch_html import get_fetch
+
+
+
+def respond_with_internet(model,temperature,memory,host="127.0.0.1",port=11434):
+    print("[NET] Requesting Searxng...")
+    prompt = " 假设你获得了联网的功能，为了给用户满意的答复，请分析刚才这段消息并输出需要进行联网搜索的关键词。只能输出关键词，使用空格分隔，不包含其余任何内容。"
+    memory_copy = [copy.deepcopy(memory)[-1],{"role":"system","content":prompt}]
+    # memory_copy[-1]["content"] = memory_copy[-1]["content"].replace("*","") + prompt
+    # memory_copy[-1]["role"] = "system"
+    url = f"http://{host}:{port}/api/chat"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "model": model,
+        "options": {
+            "temperature": temperature
+        },
+        "stream": False,
+        "messages": memory_copy
+    }
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        print("[INFO] \033[30;42m 200 \033[0m Succeed.")
+    else:
+        print("[ERROR] \033[30;41m {} \033[0m Failed when first request in func::respond_with_internet.".format(response.status_code))
+        return None, None
+    result = response.text
+    if "deepseek" in model:
+        result = result[result.find(r"/think\u003e\n\n") + len(r"/think\u003e\n\n"):result.find("\"},\"")]
+        result = remove_markdown(result).split(" ")
+        result = [remove_markdown(s).replace(" ","") for s in result]
+    else:
+        result = remove_markdown(result[result.find("\"content\":\"") + len("\"content\":\""):].split("\"")[0]).split(" ")
+        result = [remove_markdown(s).replace(" ", "") for s in result]
+    search_ans = get_fetch(result)
+    if search_ans == {}:
+        return respond(model,temperature,memory,host=host,port=port)
+    memory_copy[-1] = {"role":"system","content":f"对用户发送的上一条信息进行了联网查询，你需要结合搜索结果用文字做出回复，搜索结果为JSON格式，搜索结果如下：{search_ans}"}
+    data = {
+        "model": model,
+        "options": {
+            "temperature": temperature
+        },
+        "stream": False,
+        "messages": memory_copy
+    }
+    second_response = requests.post(url, json=data, headers=headers)
+    if second_response.status_code == 200:
+        print("[INFO] \033[30;42m 200 \033[0m Succeed.")
+    else:
+        print("[ERROR] \033[30;41m {} \033[0m Failed when second request in func::respond_with_internet.".format(second_response.status_code))
+        return None, None
+    result = second_response.text
+    if "deepseek" in model:
+        thought = remove_markdown(
+            result[result.find(r"\u003cthink\u003e") + len(r"\u003cthink\u003e"):result.find(r"\u003c/think\u003e")])
+        result = result[result.find(r"/think\u003e\n\n") + len(r"/think\u003e\n\n"):result.find("\"},\"")]
+        result = remove_markdown(result)
+    else:
+        thought = ""
+        result = remove_markdown(result[result.find("\"content\":\"") + len("\"content\":\""):].split("\"")[0])
+    return result, thought
+
 
 def respond(model,temperature,memory,host="127.0.0.1",port=11434):
     # print("[INFO] Model:{}\n[INFO] Temperature:{}\n[INFO] Host:{}\n[INFO] Port:{}".format(model,temperature,host,port))
@@ -17,11 +81,16 @@ def respond(model,temperature,memory,host="127.0.0.1",port=11434):
     if resp.status_code == 200:
         print("[INFO] \033[30;42m 200 \033[0m Succeed.")
     else:
-        print("[ERROR] \033[30;42m {} \033[0m Failed".format(resp.status_code))
+        print("[ERROR] \033[30;41m {} \033[0m Failed".format(resp.status_code))
+        return None, None
     result = resp.text
-    thought = remove_markdown(result[result.find(r"\u003cthink\u003e")+len(r"\u003cthink\u003e"):result.find(r"\u003c/think\u003e")])
-    result = result[result.find(r"/think\u003e\n\n") + len(r"/think\u003e\n\n"):result.find("\"},\"")]
-    result = remove_markdown(result)
+    if "deepseek" in model:
+        thought = remove_markdown(result[result.find(r"\u003cthink\u003e")+len(r"\u003cthink\u003e"):result.find(r"\u003c/think\u003e")])
+        result = result[result.find(r"/think\u003e\n\n") + len(r"/think\u003e\n\n"):result.find("\"},\"")]
+        result = remove_markdown(result)
+    else:
+        thought = ""
+        result = remove_markdown(result[result.find("\"content\":\"")+len("\"content\":\""):].split("\"")[0])
     return result,thought
 
 def remove_markdown(text):
@@ -33,3 +102,10 @@ def remove_markdown(text):
     text = text.replace("#","")
     text = text.replace("\\","")
     return text
+
+if __name__ == "__main__":
+    # model = "qwen:4b"
+    # response = respond(model,temperature=0.0,memory=[{"role":"user","content":"你好"},{"role":"assistant","content":"你好！很高兴能为你提供帮助。请问有什么问题需要我回答呢？"},{"role":"user","content":"请问你怎么看待AI的？"}],host="127.0.0.1",port=11434)
+    model = "deepseek-r1:1.5b"
+    # response1 = respond(model,0.0,[{"role":"user","content":"无畏契约是什么游戏"}])
+    response = respond_with_internet(model,0.0,[{"role":"user","content":"无畏契约是什么游戏"}])
